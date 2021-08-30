50267f2beca151fa62c1933f0638bf7a88663bc1
Phoebe Dijour <phoebe.dijour@duke.edu>
2021-08-30 12:41:35 -0400

    This is how you make returns

--- returns.py
+++ returns.py
@@ -1,2 +1,14 @@
 def add(a,b):
-c = a + b
+    c = a + b
+    d = a - b
+    if c < 0:
+        return #return stops the function if there's no variable, can also return a string
+    return c, d
+
+answer = add(3,5)
+print(answer[0])
+print(answer[1])
+
+added, subtracted = add(3,5)
+print(added)
+print(subtracted)
