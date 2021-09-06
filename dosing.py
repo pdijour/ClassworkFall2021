da9b0a33c1879949e934f5ee67f50be64e3373e0
Phoebe Dijour <phoebe.dijour@duke.edu>
2021-09-06 12:43:16 -0400

    Created modular code to determine correct dosing for patient weight

---
+++ dosing.py
@@ -0,0 +1,59 @@
+# dosing.py
+
+
+"""dosing.py
+    Example program of calculating first-day dose of medicine for pediatric
+        patients.
+    NOTE:  This is a programming example, and should not be used for any
+             type of medical treatment or diagnostics.
+"""
+
+
+#def dose_amount():
+#    diagnosis = diagnosis_input()
+#    weight = weight_input_fn()
+#    dosages(diagnosis,weight)
+
+
+def inputs():
+    print("Day One Dosing Guidelines")
+    print("")
+    print("Choose diagnosis:")
+    print("1 - Acute otitis media")
+    print("2 - Acute bacterial sinusitis")
+    print("3 - Community-acquired pneumonia")
+    print("4 - Pharyngitis/tonsilitis")
+    diagnosis = int(input("Enter a number: "))
+    print("PATIENT WEIGHT")
+    print("Enter patient weight followed by units of kg or lb.")
+    print("Examples:  65.3 lb      21.0 kg")
+    weight_input = input("Enter weight: ")
+    return diagnosis, weight_input
+
+def weight_conversion(weight_input):
+    weight_data = weight_input.split(" ")
+    weight = float(weight_data[0])
+    units = weight_data[1]
+    if units == "lb":
+        weight = weight / 2.205
+    return weight
+
+def dosages(diagnosis, weight):
+    dosages_mg_per_kg = [30, 10, 10, 12]
+    dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
+    dosage_mg_first_day = weight * dosage_mg_per_kg
+    return dosage_mg_first_day
+
+def output(dosage_mg_first_day):
+    print("CORRECT DOSAGE")
+    print("For a patient weighing {:.1f} kg,".format(weight))
+    print("  the correct dosage is {:.1f} mg the first day"
+          .format(dosage_mg_first_day))
+
+
+if __name__ == '__main__':
+    #dose_amount()
+    diagnosis, weight_input = inputs()
+    weight = weight_conversion(weight_input)
+    dosage_mg_first_day = dosages(diagnosis,weight)
+    output(dosage_mg_first_day)
