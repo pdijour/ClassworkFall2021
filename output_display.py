c4b7497d991746aa676fa6f4912583d6ec9f4f05
Phoebe Dijour <phoebe.dijour@duke.edu>
2021-09-06 12:04:29 -0400

    Adding code for merging class

---
+++ output_display.py
@@ -0,0 +1,7 @@
+# output_display.py
+weight = 20 / 2.205
+dosage = weight * 30
+
+print("CORRECT DOSAGE")
+print("For a patient weighing {} kg,".format(weight))
+print("  the correct dosage is {} mg the first day".format(dosage))
